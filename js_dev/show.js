c4a4966136d7ab948fc513bf72cbce03470e0cfd
Kruno Knego <k.knego@gmail.com>
2015-03-12 16:22:19 +0100

    Minified.

---
+++ js_dev/show.js
@@ -0,0 +1,8 @@
+function showHide() {
+    if (document.getElementById("trigger-menu").style.display != "block"){
+        document.getElementById("trigger-menu").style.display = "block";
+    }
+    else {
+        document.getElementById("trigger-menu").style.display = "none";
+    }
+}
