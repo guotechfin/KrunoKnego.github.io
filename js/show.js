65ad7507ca6073db7783f70d790796c244b5481c
Kruno Knego <k.knego@gmail.com>
2015-03-21 19:04:04 +0100

    Javascript menu.

--- js/show.js
+++ js/show.js
@@ -1,5 +1,5 @@
 function showHide() {
-    if (document.getElementById("trigger-menu").style.display != "block"){
+    if (document.getElementById("svg-trigger").style.display != "block"){
         document.getElementById("trigger-menu").style.display = "block";
     }
     else {
